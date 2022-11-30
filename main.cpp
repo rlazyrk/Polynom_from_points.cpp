2b869bf9bd6b8a193e994eefce8f8f02ce3680bf
rlazyrk <rlazyrk@gmail.com>
2022-11-30 21:48:55 +0200

    changed the coordinates in points

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "classLaba5.h"
 int main() {
-    Point uno(100,7);
-    Point dos(25,17);
-    Point tres(40,28);
-    Point quatro(18, 35);
+    Point uno(35,8);
+    Point dos(11,94);
+    Point tres(55,42);
+    Point quatro(16, 22);
     Polynom x(BLUE);
     x.add(uno);
     x.add(dos);
